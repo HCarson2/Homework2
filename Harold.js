68435d6026417d18ac5df6800ecf77541267945d
HCarson2 <mair0610@gmail.com>
2022-04-07 23:38:55 -0400

    first commit

---
+++ Harold.js
@@ -0,0 +1,19 @@
+let bigPie = "18.00"
+let regularPie = "12.00"
+let medPie = "9.00"
+let beer = "3.00"
+let a = bigPie
+let b = regularPie
+let c = medPie
+let d = beer
+
+console.log(a+d)
+
+let customerFavorite = ['medPie',
+'bigPie',
+'beer']
+
+console.log(customerFavorite)
+
+for( i = 0; i < 20; i++) {"I love Harold's Pizza"
+console.log(i)}
